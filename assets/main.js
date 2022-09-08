03b1e08b4c181e4aca9bd7db0f568f78195db1c8
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2022-09-08 12:26:47 +0200

    feat: create element adding  with keyframes zooming animation

--- assets/main.js
+++ assets/main.js
@@ -10,14 +10,25 @@ const habits = [
     }
 ]
 
+const toggleHabit = (index) => {
+    render([
+        ...habits,
+        {
+            img: "./assets/img/habit_1.svg",
+            name: "Task 2",
+            completed: [true, false, true, false, false, false, false]
+        }
+    ])
+}
+
 const getWeekDaysElement = (completed) => weekDays.map((name, index) => completed[index] ?
-    `<button class="button-checked">
+    `<button class="button-checked" onClick="toggleHabit(${index})">
             <img src="./assets/img/done_icon.svg" alt="done" width="30" />
         </button>`
-    : `<button>${name}</button>`
+    : `<button onClick="toggleHabit(${index})">${name}</button>`
 ).join('')
 
-const getHabitElement = ({ img, name, completed }) => `<div class="mb-8">
+const getHabitElement = ({ img, name, completed }) => `<div class="mb-8 habit">
 <div class="habit-header">
   <img src="${img}" alt="habit_1" width="100" />
   <span class="font-semibold text-2xl">${name}</span>
@@ -27,5 +38,11 @@ const getHabitElement = ({ img, name, completed }) => `<div class="mb-8">
   </div>
 </div>`
 
-const habitContainer = document.querySelector(".habit-container")
-habitContainer.innerHTML = habits.map(habit => getHabitElement(habit)).join('')
+const render = (habits) => {
+    const habitContainer = document.querySelector(".habit-container")
+    habitContainer.innerHTML = habits.map(habit => getHabitElement(habit))
+        .join('')
+}
+
+
+render(habits)
